d3eb51bb6296cab929a196bc490c7a8588e9ba97
Evlakhovych Ihor Vitaliyovych <127222921+1Mk7i@users.noreply.github.com>
2024-02-17 10:58:19 +0200

    update

--- src/js/index.js
+++ src/js/index.js
@@ -19,6 +19,7 @@ function fetchAndDisplayContent(url, codeId, textId) {
 fetchAndDisplayContent('OOP-res/tasks/task1.cpp', 'Prak1OOPnum1');
 fetchAndDisplayContent('OOP-res/tasks/task2.cpp', 'Prak1OOPnum2');
 fetchAndDisplayContent('OOP-res/tasks/lab1.cpp', 'Lab1OOP');
+//fetchAndDisplayContent('OOP-res/tasks/lab2.cpp', 'Lab2OOP');
 fetchAndDisplayContent('OOP-res/tasks/Prak2OOPS1.cpp','Prak2OOPnum1')
 fetchAndDisplayContent('OOP-res/tasks/Prak2OOPMain.cpp','Prak2OOPnum2')
 fetchAndDisplayContent('OOP-res/tasks/Prak2OOPS3.cpp','Prak2OOPnum3')
