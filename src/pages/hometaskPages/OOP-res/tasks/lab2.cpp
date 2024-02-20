ce54f782e15d20c2a97145743af6cfe8ca94cf21
Evlakhovych Ihor Vitaliyovych <127222921+1Mk7i@users.noreply.github.com>
2024-02-20 13:56:08 +0200

    update

--- src/pages/hometaskPages/OOP-res/tasks/lab2.cpp
+++ src/pages/hometaskPages/OOP-res/tasks/lab2.cpp
@@ -1,95 +1,127 @@
-// Спроектувати клас, що описує задану сутність. Всі властивості класу повинні бути описаними в закритій частині. Деякі з властивостей мають бути структурами.
-// Реалізувати конструктор без аргументів та один чи декілька конструкторів ініціалізації з параметрами. 
-// Створити методи для зміни властивостей класу (сетери) та отримання значення властивостей (гетери). Частина методів повинна мати реалізацію поза класом.
-// Реалізувати метод(и), що в якості параметру приймає об’єкт даного класу. 
-// В програмі обов’язково слід продемонструвати різні способи створення об’єктів - екземплярів класу та масиву об’єктів, їх ініціалізації та використання всіх методів класу.
-
 #include <iostream>
+#include <iomanip>
 #include <vector>
 #include <string>
 
 using namespace std;
 
 class TParcer {
-    private:
-        string str = "2 % 5";
-    public:
-        // розбиття рядка на числа та знаки
-        void parse() {
-            vector<int> numbers;
-            vector<char> signs;
-            string temp = "";
-            for (int i = 0; i < str.length(); i++) {
-                if (str[i] == '+' || str[i] == '-' || str[i] == '*' || str[i] == '/' || str[i] == '%') {
-                    numbers.push_back(stoi(temp));
-                    temp = "";
-                    signs.push_back(str[i]);
-                } else {
-                    temp += str[i];
-                }
-            }
-            numbers.push_back(stoi(temp));
-            for (int i = 0; i < numbers.size(); i++) {
-                cout << numbers[i] << " ";
-            }
-            cout << endl;
-            for (int i = 0; i < signs.size(); i++) {
-                cout << signs[i] << " ";
-            }
-            cout << endl;
-        }
+private:
+    string str;
+    const int zeroAfterComma = 2;
+public:
+    TParcer() {
+        cout << "Введіть рядок: ";
+        cin >> str;
+    }
 
-        void calculate() {
-            vector<int> numbers;
-            vector<char> signs;
-            string temp = "";
-            for (int i = 0; i < str.length(); i++) {
-                if (str[i] == '+' || str[i] == '-' || str[i] == '*' || str[i] == '/' || str[i] == '%') {
-                    numbers.push_back(stoi(temp));
-                    temp = "";
-                    signs.push_back(str[i]);
-                } else {
-                    temp += str[i];
-                }
-                cout << temp << endl;
+    TParcer(string str) {
+        this->str = str;
+    }
+
+    void Display(){
+        cout << "\033[1;34m" << "Вираз: " << str << "\033[0m" << endl;
+    }
+    // розбиття рядка на числа та знаки
+    void parse() {
+        vector<int> numbers;
+        vector<char> signs;
+        string temp = "";
+        for (int i = 0; i < str.length(); i++) {
+            if (str[i] == '+' || str[i] == '-' || str[i] == '*' || str[i] == '/' || str[i] == '%') {
+                numbers.push_back(stoi(temp));
+                temp = "";
+                signs.push_back(str[i]);
+            } else {
+                temp += str[i];
             }
-            numbers.push_back(stoi(temp));
-            for (int i = 0; i < signs.size(); i++) {
-                if (signs[i] == '*') {
-                    numbers[i] *= numbers[i + 1];
-                    numbers.erase(numbers.begin() + i + 1);
-                    signs.erase(signs.begin() + i);
-                    i--;
-                } else if (signs[i] == '/') {
-                    numbers[i] /= numbers[i + 1];
-                    numbers.erase(numbers.begin() + i + 1);
-                    signs.erase(signs.begin() + i);
-                    i--;
-                }
-            }
-            for (int i = 0; i < numbers.size(); i++) {
-                if (signs[i] == '%') {
-                    numbers[i] %= numbers[i + 1];
-                    numbers.erase(numbers.begin() + i + 1);
-                    signs.erase(signs.begin() + i);
-                    i--;
-                }
-            }
-            for (int i = 0; i < signs.size(); i++) {
-                if (signs[i] == '+') {
-                    numbers[i] += numbers[i + 1];
-                } else if (signs[i] == '-') {
-                    numbers[i] -= numbers[i + 1];
-                }
-            }
-            cout << numbers[0] << endl;
         }
+        numbers.push_back(stoi(temp));
+        for (int i = 0; i < numbers.size(); i++) {
+            cout << numbers[i] << " ";
+        }
+        cout << endl;
+        for (int i = 0; i < signs.size(); i++) {
+            cout << signs[i] << " ";
+        }
+        cout << endl;
+    }
+
+    void calculate() {
+        vector<double> numbers;
+        vector<char> signs;
+        string temp = "";
+        for (int i = 0; i < str.length(); i++) {
+            if (str[i] == '+' || str[i] == '-' || str[i] == '*' || str[i] == '/' || str[i] == '%') {
+                numbers.push_back(stoi(temp));
+                temp = "";
+                signs.push_back(str[i]);
+            } else {
+                temp += str[i];
+            }
+        }
+        numbers.push_back(stoi(temp));
+        for (int i = 0; i < signs.size(); i++) {
+            if (signs[i] == '*') {
+                numbers[i] = numbers[i] * numbers[i + 1];
+                numbers.erase(numbers.begin() + i + 1);
+                signs.erase(signs.begin() + i);
+                i--;
+            } else if (signs[i] == '/') {
+                if (numbers[i + 1] != 0 && numbers[i] != 0) {
+                    numbers[i] = numbers[i] / numbers[i + 1];
+                    numbers.erase(numbers.begin() + i + 1);
+                    signs.erase(signs.begin() + i);
+                    i--;
+                } else {
+                    cout << "Error: Не можна ділити на нуль!" << endl;
+                    return;
+                }
+            } else if (signs[i] == '%' && numbers[i] != 0) {
+                if (numbers[i + 1] != 0) {
+                    numbers[i] = static_cast<int>(numbers[i]) % static_cast<int>(numbers[i + 1]);
+                    numbers.erase(numbers.begin() + i + 1);
+                    signs.erase(signs.begin() + i);
+                    i--;
+                } else {
+                    cout << "Error: Не можна ділити на нуль!" << endl;
+                    return;
+                }
+            }
+        }
+        for (int i = 0; i < signs.size(); i++) {
+            if (signs[i] == '+') {
+                numbers[i] = numbers[i] + numbers[i + 1];
+            } else if (signs[i] == '-') {
+                numbers[i] = numbers[i] - numbers[i + 1];
+            }
+        }
+        // Виводимо результат з точністю до 2 знаків після коми
+        cout << fixed << setprecision(zeroAfterComma) << "\033[1;32m" << "Результат: " <<  "\033[1;33m" << numbers[0] << "\033[0m" << endl;
+    }
 };
 
 int main() {
-    TParcer parcer;
-    parcer.parse();
-    parcer.calculate();
+    vector<string> tasks = {"2+2", "2-2", "2*2", "2/2", "2%2", "2+2*2", "2*2+2", "2+2/2", "2/2+2", "2+2%2", "2%2+2", "2-2*2", "2*2-2", "2-2/2", "2/2-2", "2-2%2", "2%2-2"};
+
+    cout << "\033[1;32m" << "Введіть:" << endl << "1 - для відправки масиву на обробку " << endl << "2 - для ручного вводу" << "\033[0m" << endl;
+    int n;
+    cin >> n;
+    if (n == 1) {
+        for (int i = 0; i < tasks.size(); i++) {
+            TParcer parcer(tasks[i]);
+            parcer.parse();
+            parcer.Display();
+            parcer.calculate();
+        }
+    } else if (n == 2) {
+        TParcer parcer;
+        parcer.parse();
+        parcer.Display();
+        parcer.calculate();
+    } else {
+        cout << "Error: Невірний ввід!" << endl;
+    }
 
     return 0;
 }
