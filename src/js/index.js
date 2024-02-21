304e317c767c3b33e9341341163a219320a5a316
Evlakhovych Ihor Vitaliyovych <127222921+1Mk7i@users.noreply.github.com>
2024-02-21 08:18:43 +0200

    update

--- src/js/index.js
+++ src/js/index.js
@@ -16,13 +16,13 @@ function fetchAndDisplayContent(url, codeId, textId) {
             textContainer.appendChild(preElement);
         });
 }
-fetchAndDisplayContent('OOP-res/tasks/task1.cpp', 'Prak1OOPnum1');
-fetchAndDisplayContent('OOP-res/tasks/task2.cpp', 'Prak1OOPnum2');
-fetchAndDisplayContent('OOP-res/tasks/lab1.cpp', 'Lab1OOP');
-fetchAndDisplayContent('OOP-res/tasks/lab2.cpp', 'Lab2OOP');
-fetchAndDisplayContent('OOP-res/tasks/Prak2OOPS1.cpp','Prak2OOPnum1')
-fetchAndDisplayContent('OOP-res/tasks/Prak2OOPMain.cpp','Prak2OOPnum2')
-fetchAndDisplayContent('OOP-res/tasks/Prak2OOPS3.cpp','Prak2OOPnum3')
+fetchAndDisplayContent('tasks/task1.cpp', 'Prak1OOPnum1');
+fetchAndDisplayContent('tasks/task2.cpp', 'Prak1OOPnum2');
+fetchAndDisplayContent('tasks/lab1.cpp', 'Lab1OOP');
+fetchAndDisplayContent('tasks/lab2.cpp', 'Lab2OOP');
+fetchAndDisplayContent('tasks/Prak2OOPS1.cpp','Prak2OOPnum1')
+fetchAndDisplayContent('tasks/Prak2OOPMain.cpp','Prak2OOPnum2')
+fetchAndDisplayContent('tasks/Prak2OOPS3.cpp','Prak2OOPnum3')
 
 // JavaScript-функція для прокручування до певного пункту
 function scrollToSection(button) {
