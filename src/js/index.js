888f610322ad990d9f82fde9bcc99c8ebd90f78a
Evlakhovych Ihor Vitaliyovych <127222921+1Mk7i@users.noreply.github.com>
2024-03-04 15:20:18 +0200

    update

--- src/js/index.js
+++ src/js/index.js
@@ -36,3 +36,20 @@ function scrollToSection(button) {
     var element = document.getElementById(targetId);
     element.scrollIntoView({ behavior: 'smooth' });
 }
+
+
+
+// Функція для завантаження вмісту з іншого HTML файлу
+function loadContent(url, targetId) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            document.getElementById(targetId).innerHTML = xhr.responseText;
+        }
+    };
+    xhr.send();
+}
+
+// Виклик функції для завантаження вмісту з іншого файлу
+loadContent('res/OOPnav.html', 'nav-content');
