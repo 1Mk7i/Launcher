91a8c19b53b5d4e0fc4de17607abf3a033e7ca45
Evlakhovych Ihor Vitaliyovych <127222921+1Mk7i@users.noreply.github.com>
2024-03-13 17:39:17 +0200

    update

--- src/pages/hometaskPages/OOP-res/tasks/prak5/Prak5OOP.cpp
+++ src/pages/hometaskPages/OOP-res/tasks/prak5/Prak5OOP.cpp
@@ -5,12 +5,12 @@
 using namespace std;
 
 int main() {
-    cout << "Програма починає виконуватися\n";
+    cout << "Program start\n";
     Point p(7.0, 8.0, 9.0);
     Vector v(5.0, 5.0, -4.0);
     p.print();
     p.moveByVector(v);
     p.print();
-    cout << "\nПрограма завершує виконуватися\n";
+    cout << "\nProgram end\n";
     return 0;
 }
