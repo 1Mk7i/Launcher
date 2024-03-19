c9010e5a75864e32ac2a22df7fcb62ea6b449a87
Evlakhovych Ihor Vitaliyovych <127222921+1Mk7i@users.noreply.github.com>
2024-03-19 21:44:54 +0200

    update

--- src/js/index.js
+++ src/js/index.js
@@ -37,3 +37,15 @@ function loadContent(url, targetId) {
     };
     xhr.send();
 }
+// display change
+function loadContentW(url, target) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById(target).innerHTML = this.responseText;
+            document.getElementById(target).style.display = "block";
+        }
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
+}
--- src/pages/hometaskPages/TDI/js/indexTDI.js
+++ src/pages/hometaskPages/TDI/js/indexTDI.js
@@ -1,4 +1,3 @@
+
 // Виклик функції для завантаження вмісту з іншого файлу
-loadContent('TDI-res/Virus.html', 'virus-content');
-loadContent('TDI-res/CiberAttack.html', 'ciber-attack-content');
 loadContent('TDI-res/TDInav.html', 'nav-content-tdi');
