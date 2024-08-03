b1108b1858c94ebfe17e7a921ff585cb87657a5a
Evlakhovych Ihor Vitaliyovych <127222921+1Mk7i@users.noreply.github.com>
2024-08-03 20:15:30 +0300

    global update

--- src/js/index.js
+++ src/js/index.js
@@ -24,17 +24,6 @@ function scrollToSection(button) {
     element.scrollIntoView({ behavior: 'smooth' });
 }
 
-// Функція для завантаження вмісту з іншого HTML файлу
-function loadContent(url, targetId) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true);
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            document.getElementById(targetId).innerHTML = xhr.responseText;
-        }
-    };
-    xhr.send();
-}
 // display change
 function loadContentW(url, target) {
     var xhttp = new XMLHttpRequest();
@@ -50,36 +39,46 @@ function loadContentW(url, target) {
 
 // викликаємо функцію для кожного елемента з атрибутом data-code
 document.addEventListener('DOMContentLoaded', function() {
-    document.querySelectorAll('[data-code]').forEach(function(element) {
-        const filePath = element.getAttribute('data-code');
-        const PathFunction = element.getAttribute('data-function');
-        // fetchAndDisplayContent(filePath, element.id);
-        // перевіряємо, яка функція викликається
-        if (PathFunction === 'loadContent') {
-            loadContent(filePath, element.id);
-        } else if (PathFunction === 'loadContentW') {
-            loadContentW(filePath, element.id);
-        } else if (PathFunction === 'fetchAndDisplayContent'){
-            fetchAndDisplayContent(filePath, element.id);
-        }
-    });
-    
-    // створюємо кнопку для кожного елемента з атрибутоми linc-data та name-data (кнопки)
-    var previews = document.querySelectorAll('.preview[link-data][name-data]');
-    previews.forEach(function(preview) {
-        var link = preview.getAttribute('link-data');
-        var text = preview.getAttribute('name-data');
-        var buttonHTML = '<div class="Button"><a class="btn btn-primary whiteb" href="' + link + '">' + text + '</a></div>';
-        preview.innerHTML = buttonHTML;
-    });
-    // створюємо кнопку для кожного елемента з атрибутоми linc-data, name-data та image-data (кнопки з картинкою)
-    var previews = document.querySelectorAll('.preview[link-data][name-data][image-data]');
-    previews.forEach(function(preview) {
-        var link = preview.getAttribute('link-data');
-        var text = preview.getAttribute('name-data');
-        var image = preview.getAttribute('image-data');
-        var buttonHTML = '<img class="previewImg" src="'+ image +'" alt="preview"><div class="Button"><a class="btn btn-primary whiteb" href="'+ link +'">'+ text +'</a></div>';
-        preview.innerHTML = buttonHTML;
+    const elements = document.querySelectorAll('[load-link]');
+
+    elements.forEach(element => {
+        const contentPath = element.getAttribute('load-link');
+
+        element.addEventListener('click', function(event) {
+            event.preventDefault();
+            loadContent(contentPath, 'content2');
+        });
     });
 });
 
+// Функція для завантаження вмісту за замовчуванням
+function loadDefaultContent() {
+    loadContent('/Launcher/src/pages/res/default.html', 'content2');
+}
+// Функція для завантаження вмісту з іншого HTML файлу
+function loadContent(url, targetId) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            document.getElementById(targetId).innerHTML = xhr.responseText;
+        }
+    };
+    xhr.send();
+
+    setTimeout(function() {
+        document.querySelectorAll('[data-code]').forEach(function(element) {
+            const filePath = element.getAttribute('data-code');
+            const PathFunction = element.getAttribute('data-function');
+            if (PathFunction === 'loadContent') {
+                loadContent(filePath, element.id);
+            } else if (PathFunction === 'loadContentW') {
+                loadContentW(filePath, element.id);
+            } else if (PathFunction === 'fetchAndDisplayContent') {
+                fetchAndDisplayContent(filePath, element.id);
+            }
+        });
+    }, 3000); // 1000 мс = 1 секунда
+}
+// Завантаження вмісту за замовчуванням при завантаженні сторінки
+window.onload = loadDefaultContent;
